199d8189426555a23572c6e288dc796d27827219
Fabrício Godoy <skarllot@gmail.com>
2016-08-19 03:17:11 -0300

    New propose to solve static inheritance

---
+++ InheritableStatic.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace InheritableStatic
+{
+	public class ObjectAttribute : Attribute
+	{
+		public string Name { get; set; }
+
+		public ObjectAttribute (string name)
+		{
+			Name = name;
+		}
+	}
+
+	public abstract class BaseObject
+	{
+		public static string GetName<T> () where T : BaseObject
+		{
+			return (from objAttr in typeof(T).GetCustomAttributes (typeof(ObjectAttribute), false)
+			       let attr = (ObjectAttribute)objAttr
+			       select attr.Name).FirstOrDefault ();
+		}
+	}
+
+	[ObjectAttribute ("A")]
+	public class AObject : BaseObject
+	{
+		public string Code { get; set; }
+
+		public int Height { get; set; }
+	}
+
+	[ObjectAttribute ("B")]
+	public class BObject : BaseObject
+	{
+		public string Code { get; set; }
+
+		public int Weight { get; set; }
+	}
+}
---
+++ UnitTests/InheritableStaticTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using InheritableStatic;
+
+namespace UnitTests
+{
+	[TestFixture ()]
+	public class InheritableStaticTests
+	{
+		[Test ()]
+		public void BaseObjectNameTest ()
+		{
+			Assert.AreEqual (null, BaseObject.GetName<BaseObject>());
+		}
+
+		[Test ()]
+		public void AObjectNameTest ()
+		{
+			Assert.AreEqual ("A", BaseObject.GetName<AObject>());
+		}
+
+		[Test ()]
+		public void BObjectNameTest ()
+		{
+			Assert.AreEqual ("B", BaseObject.GetName<BObject>());
+		}
+	}
+}
+

2ce730deecad76af26881e8db3546ec271e603ca
Fabricio Godoy <skarllot@gmail.com>
2016-09-17 02:19:38 -0300

    New code to retrieve internal NET Resource

---
+++ EnvironmentResource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace FrameworkInternalResource
+{
+    public static class EnvironmentResource
+    {
+        private const BindingFlags ReflectionFilter = BindingFlags.Static | BindingFlags.NonPublic;
+        private static readonly Func<object, object[], object> GetResourceString = (from m in typeof(Environment).GetMethods(ReflectionFilter)
+                                                                where m.Name == "GetResourceString"
+                                                                select m).First().Invoke;
+
+        public static string GetString(string key, string cultureName)
+        {
+            var ci = new System.Globalization.CultureInfo(cultureName);
+            var thread = System.Threading.Thread.CurrentThread;
+            var originalCulture = thread.CurrentCulture;
+            var originalUICulture = thread.CurrentUICulture;
+            thread.CurrentCulture = ci;
+            thread.CurrentUICulture = ci;
+
+            var value = (string)GetResourceString(null, new object[] { key });
+
+            thread.CurrentUICulture = originalUICulture;
+            thread.CurrentCulture = originalCulture;
+            return value;
+        }
+    }
+}
---
+++ UnitTests/EnvironmentResourceTests.cs
@@ -0,0 +1,29 @@
+﻿using FrameworkInternalResource;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class EnvironmentResourceTests
+    {
+        const string EnUs = "en-US";
+        const string PtBr = "pt-BR";
+        const string AggregateException_ctor_DefaultMessage = "AggregateException_ctor_DefaultMessage";
+        const string AggregateException_ctor_InnerExceptionNull = "AggregateException_ctor_InnerExceptionNull";
+        const string AggregateException_DeserializationFailure = "AggregateException_DeserializationFailure";
+        const string AggregateException_ToString = "AggregateException_ToString";
+
+        [TestCase(AggregateException_ctor_DefaultMessage, EnUs, Result = "One or more errors occurred.")]
+        [TestCase(AggregateException_ctor_DefaultMessage, PtBr, Result = "Um ou mais erros.")]
+        [TestCase(AggregateException_ctor_InnerExceptionNull, EnUs, Result = "An element of innerExceptions was null.")]
+        [TestCase(AggregateException_ctor_InnerExceptionNull, PtBr, Result = "Um elemento de innerExceptions era nulo.")]
+        [TestCase(AggregateException_DeserializationFailure, EnUs, Result = "The serialization stream contains no inner exceptions.")]
+        [TestCase(AggregateException_DeserializationFailure, PtBr, Result = "O fluxo da serialização não contém exceções internas.")]
+        [TestCase(AggregateException_ToString, EnUs, Result = "{0}{1}---> (Inner Exception #{2}) {3}{4}{5}")]
+        [TestCase(AggregateException_ToString, PtBr, Result = "{0}{1}---> (Exceção Interna N° {2}) {3}{4}{5}")]
+        public string GetStringTest(string key, string cultureName)
+        {
+            return EnvironmentResource.GetString(key, cultureName);
+        }
+    }
+}

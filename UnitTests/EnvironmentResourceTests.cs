64dcb48920a2a48287e93dad5de76efbc9096f4c
Fabricio Godoy <skarllot@gmail.com>
2016-09-19 02:43:31 -0300

    New Task tests

--- UnitTests/EnvironmentResourceTests.cs
+++ UnitTests/EnvironmentResourceTests.cs
@@ -13,6 +13,8 @@ namespace UnitTests
         const string AggregateException_DeserializationFailure = "AggregateException_DeserializationFailure";
         const string AggregateException_ToString = "AggregateException_ToString";
         const string Task_Dispose_NotCompleted = "Task_Dispose_NotCompleted";
+        const string SpinWait_SpinUntil_TimeoutWrong = "SpinWait_SpinUntil_TimeoutWrong";
+        const string SpinWait_SpinUntil_ArgumentNull = "SpinWait_SpinUntil_ArgumentNull";
 
         [TestCase(AggregateException_ctor_DefaultMessage, EnUs, Result = "One or more errors occurred.")]
         [TestCase(AggregateException_ctor_DefaultMessage, PtBr, Result = "Um ou mais erros.")]
@@ -24,6 +26,10 @@ namespace UnitTests
         [TestCase(AggregateException_ToString, PtBr, Result = "{0}{1}---> (Exceção Interna N° {2}) {3}{4}{5}")]
         [TestCase(Task_Dispose_NotCompleted, EnUs, Result = "A task may only be disposed if it is in a completion state (RanToCompletion, Faulted or Canceled).")]
         [TestCase(Task_Dispose_NotCompleted, PtBr, Result = "Uma tarefa só pode ser descartada se estiver em estado de conclusão (RanToCompletion, Faulted ou Canceled).")]
+        [TestCase(SpinWait_SpinUntil_TimeoutWrong, EnUs, Result = "The timeout must represent a value between -1 and Int32.MaxValue, inclusive.")]
+        [TestCase(SpinWait_SpinUntil_TimeoutWrong, PtBr, Result = "O tempo limite deve representar um valor entre -1 e Int32.MaxValue, inclusive.")]
+        [TestCase(SpinWait_SpinUntil_ArgumentNull, EnUs, Result = "The condition argument is null.")]
+        [TestCase(SpinWait_SpinUntil_ArgumentNull, PtBr, Result = "O argumento de condição é nulo.")]
         public string GetStringTest(string key, string cultureName)
         {
             return EnvironmentResource.GetString(key, cultureName);
---
+++ UnitTests/TaskTests.cs
@@ -0,0 +1,113 @@
+﻿using NUnit.Framework;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestFixture(Category = "Task")]
+    public class TaskTests
+    {
+        const int Timeout = 150;
+        const string ExceptionMessage = "Task Exception";
+        const string TaskExecutionTimeout = "Task execution could not be completed into expected time";
+
+        [Test]
+        public async Task TaskContinueExceptionTest()
+        {
+            var task = Task.Factory
+                .StartNew(() => { throw new Exception(ExceptionMessage); })
+                .ContinueWith(t =>
+                {
+                    Assert.IsTrue(t.IsFaulted);
+                });
+            await task;
+        }
+
+        [Test]
+        [ExpectedException(typeof(AggregateException))]
+        public void TaskWaitExceptionTest()
+        {
+            var task = Task.Factory.StartNew(() =>
+            {
+                Task.Delay(Timeout).Wait();
+                throw new Exception(ExceptionMessage);
+            });
+            if (!task.Wait(Timeout * 2))
+                Assert.Fail(TaskExecutionTimeout);
+        }
+
+        [Test]
+        [ExpectedException(typeof(AggregateException))]
+        public void TaskResultExceptionStarted()
+        {
+            var task = Task.Factory.StartNew<int>(() => { throw new Exception(ExceptionMessage); });
+            Assert.AreEqual(0, task.Result);
+        }
+
+        [Test]
+        public void TaskResultCreatedTest()
+        {
+            var outerTask = Task.Run(() =>
+            {
+                var task = new Task<int>(() => 15);
+                return task.Result;
+            });
+            Assert.IsFalse(outerTask.Wait(Timeout));
+        }
+
+        [Test]
+        public async Task TaskAwaitCreatedTest()
+        {
+            int counter = 0;
+            var task = new Task(() => Interlocked.Increment(ref counter));
+            if (await Task.WhenAny(task, Task.Delay(Timeout)) == task)
+                Assert.Fail("Awaiting for task should not start it");
+
+            Assert.AreEqual(0, counter);
+            Assert.AreEqual(TaskStatus.Created, task.Status);
+        }
+
+        [Test]
+        public void TaskWaitCreatedTest()
+        {
+            int counter = 0;
+            var task = new Task(() => Interlocked.Increment(ref counter));
+            if (task.Wait(Timeout))
+                Assert.Fail("Waiting for task should not start it");
+
+            Assert.AreEqual(0, counter);
+            Assert.AreEqual(TaskStatus.Created, task.Status);
+        }
+
+        [Test]
+        public void TaskWaitStartedTest()
+        {
+            int counter = 0;
+            var task = new Task(() => Interlocked.Increment(ref counter));
+
+            task.Start();
+            //Assert.AreEqual(TaskStatus.WaitingToRun, task.Status);
+
+            if (!task.Wait(Timeout))
+                Assert.Fail(TaskExecutionTimeout);
+
+            Assert.AreEqual(1, counter);
+            Assert.AreEqual(TaskStatus.RanToCompletion, task.Status);
+        }
+
+        [Test]
+        public void TaskRunSyncExceptionTest()
+        {
+            var task = new Task(() => { throw new Exception(ExceptionMessage); });
+            task.RunSynchronously();
+        }
+
+        [Test]
+        public void TaskContinueCreatedTest()
+        {
+            var task = new Task(() => { });
+            Assert.IsFalse(task.ContinueWith(t => { }).Wait(Timeout));
+        }
+    }
+}

844a7a35885bd8a047dde06730f7ee5442a14639
Fabrício Godoy <fabricio@verion.ind.br>
2016-12-16 17:39:20 -0200

    New generic variance tests

---
+++ UnitTests/Variance.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using Variance;
+
+namespace UnitTests
+{
+    [TestFixture(Category = "Variance")]
+    public class Variance
+    {
+        void PrintAnimals(IEnumerable<Animal> animals, System.IO.TextWriter writer)
+        {
+            foreach (var animal in animals)
+                writer.WriteLine(animal.Name);
+        }
+
+        void CompareCats(IComparer<Cat> comparer, System.IO.TextWriter writer)
+        {
+            var cat1 = new Cat("Otto");
+            var cat2 = new Cat("Troublemaker");
+
+            if (comparer.Compare(cat2, cat1) > 0)
+                writer.WriteLine("Troublemaker wins!");
+        }
+
+        void CallWithPrinter(Action<Action<Animal>> animalGenerator, System.IO.TextWriter writer)
+        {
+            animalGenerator(animal => writer.WriteLine(animal.Name));
+        }
+
+        [Test]
+        public void Covariance()
+        {
+            var writer = new System.IO.StringWriter();
+            IEnumerable<Cat> cats = new List<Cat> { new Cat("Troublemaker") };
+            PrintAnimals(cats, writer);
+            Assert.AreEqual("Troublemaker" + Environment.NewLine, writer.ToString());
+        }
+
+        [Test]
+        public void Contravariance()
+        {
+            var writer = new System.IO.StringWriter();
+            IComparer<Animal> compareAnimals = new AnimalSizeComparator();
+            CompareCats(compareAnimals, writer);
+            Assert.AreEqual("Troublemaker wins!" + Environment.NewLine, writer.ToString());
+        }
+
+        [Test]
+        public void VarianceRevert()
+        {
+            var writer = new System.IO.StringWriter();
+            Action<Action<Cat>> catGenerator = handler => handler(new Cat("Otto"));
+            CallWithPrinter(catGenerator, writer);
+            Assert.AreEqual("Otto" + Environment.NewLine, writer.ToString());
+        }
+    }
+}
---
+++ Variance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variance
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+    }
+
+    public class Cat : Animal
+    {
+        public Cat(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class Fish : Animal
+    { }
+
+    public class Random
+    { }
+
+    public class AnimalSizeComparator : IComparer<Animal>
+    {
+        public int Compare(Animal x, Animal y)
+        {
+            return x.Name.CompareTo(y.Name);
+        }
+    }
+}
